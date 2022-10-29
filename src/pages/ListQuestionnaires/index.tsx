05546aec84f1b61a4a43ad7f58a95665fef400e1
Francisco Pereira <fran.p.s@hotmail.com>
2022-10-28 21:49:32 -0300

    feat: add page to list questionnaires

--- src/pages/ListQuestionnaires/index.tsx
+++ src/pages/ListQuestionnaires/index.tsx
@@ -1,64 +1,54 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { findAllQuestionnaire } from "../../api/questionnaire";
 import { Main } from "../../components/Main";
+import { QuestionnaireResponseType } from "../../types/questionnaire";
+import { routes_constraints, routes_helpers } from "../../util/route_utils";
 import "./styles.css";
 
 type ListQuestionnairesProps = {};
 
 export const ListQuestionnaires = ({}: ListQuestionnairesProps) => {
+  const [questionnaires, setQuestionnaires] = useState<
+    QuestionnaireResponseType[]
+  >([]);
+  const startComponent = async () => {
+    const questionnaireResult = await findAllQuestionnaire();
+    setQuestionnaires(questionnaireResult);
+  };
+  const navigate = useNavigate();
+  useEffect(() => {
+    startComponent();
+  }, []);
   return (
     <Main title="Questionários">
       <h1 className="questionarios-title">Todos ativos</h1>
       <ul className="questionarios-list">
-        <li>
-          <Link to={""}>
-            <p>Eleições para presidente</p>
-          </Link>
-          <section className="buttons-section">
-            <button type="button" className="clone">
-              Duplicar
-            </button>
-            <button type="button" className="update">
-              Atualizar
-            </button>
-            <button type="button" className="remove">
-              Excluir
-            </button>
-          </section>
-        </li>
-        <li>
-          <Link to={""}>
-            <p>Eleições para presidente</p>
-          </Link>
-          <section className="buttons-section">
-            <button type="button" className="clone">
-              Duplicar
-            </button>
-            <button type="button" className="update">
-              Atualizar
-            </button>
-            <button type="button" className="remove">
-              Excluir
-            </button>
-          </section>
-        </li>
-        <li>
-          <Link to={""}>
-            <p>Eleições para presidente</p>
-          </Link>
-          <section className="buttons-section">
-            <button type="button" className="clone">
-              Duplicar
-            </button>
-            <button type="button" className="update">
-              Atualizar
-            </button>
-            <button type="button" className="remove">
-              Excluir
-            </button>
-          </section>
-        </li>
+        {questionnaires.map((questionnaire) => (
+          <li key={questionnaire.id}>
+            <Link to={routes_helpers.mountQuestionnaireGet(questionnaire.id)}>
+              <p>{questionnaire.name}</p>
+            </Link>
+            <section className="buttons-section">
+              <button type="button" className="clone">
+                Duplicar
+              </button>
+              <button type="button" className="update">
+                Atualizar
+              </button>
+              <button type="button" className="remove">
+                Excluir
+              </button>
+            </section>
+          </li>
+        ))}
       </ul>
-      <button className="add">+</button>
+      <button
+        className="add"
+        onClick={() => navigate(routes_constraints.QUESTIONNAIRE_CREATE)}
+      >
+        +
+      </button>
     </Main>
   );
 };
