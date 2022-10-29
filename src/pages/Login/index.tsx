70f1686436ab80c896d190d6a8f4e9813be73f81
Francisco Pereira <fran.p.s@hotmail.com>
2022-10-28 21:50:36 -0300

    feat: add Login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import { doLogin } from "../../api/signin/login";
 import "./styles.css";
 
 function Login() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="Login">
-      <img className="logo" alt="Logo" src="images/logo.png" />
+      <img className="logo" alt="Logo" src="/images/logo.png" />
       <form className="login-form">
         <p>
           <label>Login:</label>
-          <input type="text" />
+          <input type="text" onChange={(e) => setLogin(e.target.value)} />
         </p>
         <p>
           <label>Senha:</label>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </p>
-        <button type="button">Entrar</button>
+        <button
+          type="button"
+          onClick={async () => {
+            await doLogin({ login, password });
+          }}
+        >
+          Entrar
+        </button>
       </form>
     </div>
   );
