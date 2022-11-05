c00d4234792f1550b34dc0c791408f1955bb1496
Francisco Pereira <fran.p.s@hotmail.com>
2022-11-05 10:46:48 -0300

    feat: add save answer option api request method

---
+++ src/api/answerOption.ts
@@ -0,0 +1,28 @@
+import { getAccessToken as getAccessTokenRepository } from "../repositories/tokenRepository";
+import {
+  AnswerOptionRequestResultType,
+  AnswerOptionRequestType,
+} from "../types/answerOption";
+import { routes_constraints } from "../util/route_utils";
+import api from "./index";
+
+const getAccessToken = (): string | null => {
+  const accessToken = getAccessTokenRepository();
+  if (!accessToken?.accessToken) return null;
+  return accessToken.accessToken;
+};
+
+const authConfig = () => ({
+  headers: { "x-access-token": `Bearer ${getAccessToken()}` },
+});
+
+export const saveAnswerOption = async (
+  answerOption: AnswerOptionRequestType
+): Promise<AnswerOptionRequestResultType> => {
+  const result = await api.post(
+    routes_constraints.ANSWER_OPTION,
+    answerOption,
+    authConfig()
+  );
+  return result.data;
+};
