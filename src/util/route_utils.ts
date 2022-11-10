655d00a76b389e1586059b6677b24e0a9fcd1cc5
Francisco Pereira <fran.p.s@hotmail.com>
2022-11-10 07:31:06 -0300

    feat: add QUESTION_ID path and some route helpers in route_utils file

--- src/util/route_utils.ts
+++ src/util/route_utils.ts
@@ -4,6 +4,7 @@ export const routes_constraints = {
   QUESTIONNAIRE_CREATE: "/questionnaires/create",
   QUESTIONNAIRE_ID: "/questionnaires/:id",
   QUESTION: "/questions",
+  QUESTION_ID: "/questions/:id",
   APPLIER: "/appliers",
   ANSWER_OPTION: "/answer_options",
   ANSWER_OPTION_ID: "/answer_options/:id",
@@ -14,4 +15,8 @@ export const routes_helpers = {
     routes_constraints.QUESTIONNAIRE_ID.replace(":id", id),
   mountQuestionIdQuestionnaire: (idQuestionnaire: string) =>
     `${routes_constraints.QUESTION}?idQuestionnaire=${idQuestionnaire}`,
+  mountQuestionId: (id: string) =>
+    `${routes_constraints.QUESTION_ID.replace(":id", id)}`,
+  mountAnswerOptionId: (id: string) =>
+    routes_constraints.ANSWER_OPTION_ID.replace(":id", id),
 };
