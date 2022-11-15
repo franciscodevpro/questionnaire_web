b15e1b1c503074277b92f8287792c56dbd339a64
Francisco Pereira <fran.p.s@hotmail.com>
2022-11-14 21:48:46 -0300

    refactor: change questionnaiare list to use context instead of call the questionnaire api

--- src/pages/ListQuestionnaires/index.tsx
+++ src/pages/ListQuestionnaires/index.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import {
-  deleteQuestionnaire,
-  findAllQuestionnaire,
-} from "../../api/questionnaire";
+import { deleteQuestionnaire } from "../../api/questionnaire";
 import { Main } from "../../components/Main";
-import { QuestionnaireResponseType } from "../../types/questionnaire";
+import MainContext from "../../contexts/questionnaire-context";
 import { confirmationPopup } from "../../util/messages";
 import { routes_constraints, routes_helpers } from "../../util/route_utils";
 import "./styles.css";
@@ -13,33 +10,27 @@ import "./styles.css";
 type ListQuestionnairesProps = {};
 
 export const ListQuestionnaires = ({}: ListQuestionnairesProps) => {
-  const [questionnaires, setQuestionnaires] = useState<
-    QuestionnaireResponseType[]
-  >([]);
-  const startComponent = async () => {
-    const questionnaireResult = await findAllQuestionnaire();
-    setQuestionnaires(questionnaireResult);
-  };
+  const { questionnaires, restartContext } = useContext(MainContext);
   const handleDeleteQuestionnaire = async (id: string) => {
     const result = confirmationPopup(
       'Tem certeza que deseja excluir o questionário: "' +
-        questionnaires.find((q) => q.id === id)?.name +
+        questionnaires?.find((q) => q.id === id)?.name +
         '" ?'
     );
     if (!result) return;
     await deleteQuestionnaire(id);
-    await startComponent();
+    await restartContext?.();
   };
   const navigate = useNavigate();
   useEffect(() => {
-    startComponent();
+    restartContext?.();
   }, []);
 
   return (
     <Main title="Questionários">
       <h1 className="questionarios-title">Todos ativos</h1>
       <ul className="questionarios-list">
-        {questionnaires.map((questionnaire) => (
+        {questionnaires?.map((questionnaire) => (
           <li key={questionnaire.id}>
             <Link to={routes_helpers.mountQuestionnaireId(questionnaire.id)}>
               <p>{questionnaire.name}</p>
