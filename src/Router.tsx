c512f17019819e8d92ac0ba5bdac6e678ea70979
Francisco Pereira <fran.p.s@hotmail.com>
2022-11-14 21:46:55 -0300

    feat: wrap routes with a main context

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,50 +1,77 @@
+import { useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { findAllQuestionnaire } from "./api/questionnaire";
+import { MainContextProvider } from "./contexts/questionnaire-context";
 import { FormQuestionnaire } from "./pages/FormQuestionnaire";
 import { ListQuestionnaires } from "./pages/ListQuestionnaires";
 import Login from "./pages/Login";
+import { QuestionnaireRequestResultType } from "./types/questionnaire";
 import { isLoggedIn } from "./util/login";
 import { routes_constraints } from "./util/route_utils";
 
 export const Router = () => {
-  const goIfLoggedIn = (ChildComponent: JSX.Element) => (
-    <>
-      {!isLoggedIn() ? (
-        <Navigate to={routes_constraints.LOGIN} />
-      ) : (
-        ChildComponent
-      )}
-    </>
-  );
+  const [questionnaires, setQuestionnaires] = useState<
+    QuestionnaireRequestResultType[]
+  >([]);
+
+  const startContext = async () => {
+    const questionnaireResult = await findAllQuestionnaire();
+    setQuestionnaires(questionnaireResult);
+  };
+
+  const goIfLoggedIn = (ChildComponent: JSX.Element) => {
+    if (isLoggedIn()) {
+      startContext();
+    }
+    return (
+      <>
+        {!isLoggedIn() ? (
+          <Navigate to={routes_constraints.LOGIN} />
+        ) : (
+          ChildComponent
+        )}
+      </>
+    );
+  };
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route
-            index
-            element={
-              <>
-                {isLoggedIn() ? (
-                  <Navigate to={routes_constraints.QUESTIONNAIRE} />
-                ) : (
-                  <Login />
-                )}
-              </>
-            }
-          />
-          <Route
-            path={routes_constraints.QUESTIONNAIRE}
-            element={goIfLoggedIn(<ListQuestionnaires />)}
-          />
-          <Route
-            path={routes_constraints.QUESTIONNAIRE_CREATE}
-            element={goIfLoggedIn(<FormQuestionnaire />)}
-          />
-          <Route
-            path={routes_constraints.QUESTIONNAIRE_ID}
-            element={goIfLoggedIn(<FormQuestionnaire type="update" />)}
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <MainContextProvider
+      value={{
+        questionnaires,
+        setQuestionnaires: (params: QuestionnaireRequestResultType[]) =>
+          setQuestionnaires(params),
+        restartContext: async () => startContext(),
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route
+              index
+              element={
+                <>
+                  {isLoggedIn() ? (
+                    <Navigate to={routes_constraints.QUESTIONNAIRE} />
+                  ) : (
+                    <Login />
+                  )}
+                </>
+              }
+            />
+            <Route
+              path={routes_constraints.QUESTIONNAIRE}
+              element={goIfLoggedIn(<ListQuestionnaires />)}
+            />
+            <Route
+              path={routes_constraints.QUESTIONNAIRE_CREATE}
+              element={goIfLoggedIn(<FormQuestionnaire />)}
+            />
+            <Route
+              path={routes_constraints.QUESTIONNAIRE_ID}
+              element={goIfLoggedIn(<FormQuestionnaire type="update" />)}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </MainContextProvider>
   );
 };
