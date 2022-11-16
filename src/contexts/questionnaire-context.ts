772ed2d6e5055c00a8035abb384118304754e812
Francisco Pereira <fran.p.s@hotmail.com>
2022-11-16 08:46:12 -0300

    refactor: replace mocked data into real data in applier menu items

--- src/contexts/questionnaire-context.ts
+++ src/contexts/questionnaire-context.ts
@@ -1,4 +1,5 @@
 import { createContext } from "react";
+import { ApplierRequestResultType } from "../types/applier";
 import { QuestionnaireRequestResultType } from "../types/questionnaire";
 
 const MainContext = createContext<{
@@ -7,7 +8,8 @@ const MainContext = createContext<{
   restartContext?: () => Promise<void>;
   openedMenuItems?: [{ [key: string]: boolean }, string?];
   setOpenedMenuItems?: (items: [{ [key: string]: boolean }, string?]) => void;
-}>({ questionnaires: [] });
+  appliers: ApplierRequestResultType[];
+}>({ questionnaires: [], appliers: [] });
 
 export const MainContextProvider = MainContext.Provider;
 export const MainContextConsumer = MainContext.Consumer;
