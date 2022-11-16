7b5e52c8352e1b72e9de2e846c766716305b3713
Francisco Pereira <fran.p.s@hotmail.com>
2022-11-16 08:44:03 -0300

    feat: add applier api methods

--- src/api/appliers.ts
+++ src/api/appliers.ts
@@ -1,8 +1,23 @@
+import { getAccessToken as getAccessTokenRepository } from "../repositories/tokenRepository";
 import { ApplierResponseType } from "../types/applier";
-import { routes_constraints } from "../util/route_utils";
+import { routes_constraints, routes_helpers } from "../util/route_utils";
 import api from "./index";
+
+const getAccessToken = (): string | null => {
+  const accessToken = getAccessTokenRepository();
+  if (!accessToken?.accessToken) return null;
+  return accessToken.accessToken;
+};
+
+const authConfig = () => ({
+  headers: { "x-access-token": `Bearer ${getAccessToken()}` },
+});
 
 export const findAllAppliers = async (): Promise<ApplierResponseType[]> => {
   const result = await api.get(routes_constraints.APPLIER);
   return result.data;
+};
+
+export const deleteApplier = async (id: string): Promise<void> => {
+  await api.delete(routes_helpers.mountApplierId(id), authConfig());
 };
