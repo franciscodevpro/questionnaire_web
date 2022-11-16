6061911a5af86083b529dccf7051548ce930dc92
Francisco Pereira <fran.p.s@hotmail.com>
2022-11-16 08:43:38 -0300

    feat: add applier route helpers

--- src/util/route_utils.ts
+++ src/util/route_utils.ts
@@ -6,6 +6,8 @@ export const routes_constraints = {
   QUESTION: "/questions",
   QUESTION_ID: "/questions/:id",
   APPLIER: "/appliers",
+  APPLIER_ID: "/appliers/:id",
+  APPLIER_CREATE: "/appliers/create",
   ANSWER_OPTION: "/answer_options",
   ANSWER_OPTION_ID: "/answer_options/:id",
 };
@@ -19,4 +21,6 @@ export const routes_helpers = {
     `${routes_constraints.QUESTION_ID.replace(":id", id)}`,
   mountAnswerOptionId: (id: string) =>
     routes_constraints.ANSWER_OPTION_ID.replace(":id", id),
+  mountApplierId: (id: string) =>
+    routes_constraints.APPLIER_ID.replace(":id", id),
 };
