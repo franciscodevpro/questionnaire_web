d1a4f8945bf83fdec5450c44c61d7959999a9108
Francisco Pereira <fran.p.s@hotmail.com>
2022-11-17 09:28:32 -0300

    refactor: change popup component to be only a wraper

--- src/components/PopUp/index.tsx
+++ src/components/PopUp/index.tsx
@@ -1,23 +1,25 @@
+import { useState } from "react";
 import "./styles.css";
 
-export const PopUp = () => {
+type PopUpProps = {
+  children: JSX.Element;
+  onClose?: () => void;
+};
+
+export const PopUp = ({ children, onClose }: PopUpProps) => {
+  const [hidden, setHidden] = useState(false);
+  const handleClick = () => {
+    setHidden(true);
+  };
   return (
-    <div className="alert-container">
-      <section className="alert-content">
-        <header>
-          Tem certeza que deseja excluir a pergunta:
-          <br />
-          <strong>A qual dos gêneros abaixo você se identifica?</strong>
-        </header>
-        <main>
-          <button className="ok" type="button">
-            Sim
-          </button>
-          <button className="cancel" type="button">
-            Cancelar
-          </button>
-        </main>
-      </section>
+    <div className={`popup-container${hidden ? " hidden" : ""}`}>
+      <div
+        className="popup-background"
+        onClick={() => {
+          onClose?.() || handleClick();
+        }}
+      ></div>
+      {children}
     </div>
   );
 };
