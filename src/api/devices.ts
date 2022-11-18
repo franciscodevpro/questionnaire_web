dc7aac5880f3a76b761387d1864427a41c9487ef
Francisco Pereira <fran.p.s@hotmail.com>
2022-11-18 19:26:43 -0300

    feat: add devices http request methods

---
+++ src/api/devices.ts
@@ -0,0 +1,34 @@
+import { getAccessToken as getAccessTokenRepository } from "../repositories/tokenRepository";
+import { DeviceRequestType, DeviceResponseType } from "../types/device";
+import { routes_constraints, routes_helpers } from "../util/route_utils";
+import api from "./index";
+
+const getAccessToken = (): string | null => {
+  const accessToken = getAccessTokenRepository();
+  if (!accessToken?.accessToken) return null;
+  return accessToken.accessToken;
+};
+
+const authConfig = () => ({
+  headers: { "x-access-token": `Bearer ${getAccessToken()}` },
+});
+
+export const findAllDevices = async (): Promise<DeviceResponseType[]> => {
+  const result = await api.get(routes_constraints.APPLIER);
+  return result.data;
+};
+
+export const saveDevice = async (data: DeviceRequestType): Promise<void> => {
+  await api.post(routes_constraints.APPLIER, data, authConfig());
+};
+
+export const updateDevice = async (
+  id: string,
+  data: DeviceRequestType
+): Promise<void> => {
+  await api.patch(routes_helpers.mountDeviceId(id), data, authConfig());
+};
+
+export const deleteDevice = async (id: string): Promise<void> => {
+  await api.delete(routes_helpers.mountDeviceId(id), authConfig());
+};
